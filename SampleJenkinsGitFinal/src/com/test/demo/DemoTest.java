42cb68b58cfd95dfa9dbc30d20bca54bde2161a1
prashjkd123 <prsh.jkd123@gmail.com>
2017-09-09 01:19:01 +0530

    First msg

---
+++ SampleJenkinsGitFinal/src/com/test/demo/DemoTest.java
@@ -0,0 +1,46 @@
+package com.test.demo;
+
+import unitee.annotations.TestClass;
+import unitee.assertions.Assertions;
+
+@TestClass
+public class DemoTest {
+
+	public void testMethod1() throws Exception {
+
+		System.out.println("This is:  testMethod1");
+		Assertions.assertTrue("", true);
+
+	}
+	
+	public void testMethod2() throws Exception {
+
+		System.out.println("This is:  testMethod2");
+		Assertions.assertTrue("", true);
+
+	}
+	
+	
+	public void testMethod3() throws Exception {
+
+		System.out.println("This is:  testMethod3");
+		Assertions.assertTrue("", true);
+
+	}
+	
+	
+	public void testMethod4() throws Exception {
+
+		System.out.println("This is:  testMethod4");
+		Assertions.assertTrue("", true);
+
+	}
+	
+	public void testMethod5() throws Exception {
+
+		System.out.println("This is:  testMethod51");
+		Assertions.assertTrue("", true);
+
+	}
+	
+}
